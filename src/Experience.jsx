44d1083640a64c202be656a34a0b4369c16c9aad
otanodesignco <otanodesign@gmail.com>
2024-11-11 01:58:39 -0600

    added scale effect

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef } from 'react'
+import {  useRef } from 'react'
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
 import Tornado from './components/Tornado.jsx'
 import TornadoInner from './components/TornadoInner.jsx'
 import TornadoOutter from './components/TornadoOutter.jsx'
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls, useProgress } from '@react-three/drei'
+import { useProgress } from '@react-three/drei'
+import { damp3 } from 'maath/easing'
 
 export default function Experience()
 {
@@ -14,6 +15,10 @@ export default function Experience()
 
     useFrame( ( state, delta ) =>{
 
+        if( progress.progress === 100 )
+        {
+            damp3(self.current.scale, 1, .25, delta) 
+        }
     })
 
     return <>
@@ -21,7 +26,7 @@ export default function Experience()
             <Bloom luminanceThreshold={ 1.1 } mipmapBlur intensity={ 1.2 } />
         </EffectComposer>
 
-        <group ref={ self }>
+        <group ref={ self } scale={ 0 }>
             <TornadoOutter scale-x={ 0.6 } scale-z={ 0.6 } scale-y={ 0.4 } position-y={ -2.5 } />
             <Tornado scale-x={ 0.44 } scale-z={ 0.44 } scale-y={ 0.4 } position-y={ -2.5 } />
             <TornadoInner scale={ 0.4 } position-y={ -2.5 } />
